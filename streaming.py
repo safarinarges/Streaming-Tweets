dcc965f68c3fcadac89bf1541b92c283ab93cd62
Apple <safarinarges>
2018-08-15 12:46:20 +1200

    Add the json file to save the live streams

--- streaming.py
+++ streaming.py
@@ -9,20 +9,38 @@ import sentimentmodul as sen_mod
 from Twitteraccount import *
 
 class Mylistener (StreamListener):
+    
     def on_data (self, data):
 #        print(data)
 #        return(True)
 #    def on_error(self, data):
 #        print(status)
+
+
         all_data = json.loads(data)
 
         tweets = all_data["text"] 
         sentiment_value, confidence = sen_mod.sentiment(tweets)
         print(tweets, sentiment_value, confidence)
+        
+        if confidence*100 >= 80:
+            output = open("python1.json","a")
+            output.write(tweets)
+            output.close()
 
 
-#auth = OAuthHandler(ckey, csecret)
-#auth.set_access_token(atoken, asecret)
-#
-#twitterStream = Stream(auth, Mylistener())
-#twitterStream.filter(track=["protest"])
+#            output = open("twitter-out.txt","a")
+#            output.write(sentiment_value)
+#            output.close()            
+            
+            
+        return True
+    def on_error(self, status):
+        print(status)
+
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+
+twitterStream = Stream(auth, Mylistener())
+twitterStream.filter(track=["protest"])
